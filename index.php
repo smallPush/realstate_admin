adbd42db5e68e10476e8578fd9751cd5a5ff88d8
Rubén Pineda <ruben.vilassar@gmail.com>
2026-04-11 23:23:25 +0200

    fix: use absolute paths in entry points for better path resolution

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
  */
 
 // If we are in the root directory, public/index.php is the real one.
-if (file_exists(__DIR__.'/public/index.php')) {
-    require_once __DIR__.'/public/index.php';
+$entryPoint = realpath(__DIR__ . '/public/index.php');
+
+if ($entryPoint) {
+    require_once $entryPoint;
 } else {
     // This part should technically never be reachable if this file is in the root,
     // but it provides a fallback just in case.
-    die('Unable to locate the Symfony entry point.');
+    die('Unable to locate the Symfony entry point (public/index.php).');
 }
--- public/index.php
+++ public/index.php
@@ -2,11 +2,23 @@
 
 use App\Kernel;
 
-if (file_exists(dirname(__DIR__).'/vendor/autoload_runtime.php')) {
-    require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-} else {
-    require_once __DIR__.'/../vendor/autoload_runtime.php';
+// Robustly find the vendor/autoload_runtime.php
+$autoloadPath = realpath(__DIR__ . '/../vendor/autoload_runtime.php');
+
+if (!$autoloadPath && file_exists(__DIR__ . '/vendor/autoload_runtime.php')) {
+    $autoloadPath = realpath(__DIR__ . '/vendor/autoload_runtime.php');
 }
+
+if (!$autoloadPath) {
+    // Last resort: check from root if included from elsewhere
+    $autoloadPath = realpath(getcwd() . '/vendor/autoload_runtime.php');
+}
+
+if (!$autoloadPath) {
+    throw new \RuntimeException('Unable to find vendor/autoload_runtime.php. Did you run "composer install"?');
+}
+
+require_once $autoloadPath;
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
